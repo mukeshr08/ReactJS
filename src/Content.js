8ff80dac283a7e7a46055ff6c2867b0177581ad4
Abishake01 <abishake381@gmail.com>
2025-08-08 13:51:43 +0530

    list and keys

--- src/Content.js
+++ src/Content.js
@@ -1,34 +1,54 @@
 import React from 'react';
 import { useState } from 'react';
+import { FaTrash } from "react-icons/fa";
 // it return an array,previous state and next state,hatja(disadvantage)-no class component,conditional operator,
 
 const Content = () => {
-      const [changename,Setchangename]=useState("grow"); 
-    function NameChange(){
-         
-
-        const arr=["grow","give","Earn"]
-        const num=Math.floor(Math.random()*3);
-        Setchangename(arr[num])  
-    }
+  const [array,setArray]=useState(
+    [
+      {id:1,checked:true,content:"coder"},
+      {id:2,checked:true,content:"cricketer"},
+      {id:3,checked:true,content:"soccer"}
+    ]
+  )
+  const changestate = (id)=>{
+    const changechecklist=array.map((item)=>
+      item.id===id?{...item,checked:!item.checked}:item
     
-
-
-
-
+    )
+    setArray(changechecklist)
+    localStorage.setItem("to_do_list",JSON.stringify(changechecklist))
+  }
+  const deletefunction=(id)=>{
+    const afterdeletelist=array.filter((item)=>
+      item.id!=id
+      
+    )
+    setArray(afterdeletelist)
+    
+  }
+    
+    
   return (
-    <div>
-      Subscribe to Mukesh
-      <p>Let's {changename} Money</p>
-      {/* it has the parameter function which is execute in the load of the website. */}
-      
-      <button onClick={NameChange}>subscribe</button>
-      {/* if we use parameter,the only way is to implement the anonymous function */}
-     
-      
+   <main>
+    {array.length?(
+      <ul>
+        {array.map((role)=>(
+          <li key={role.id}>
+            <input type="checkbox"
+            checked={role.checked} onChange={()=>changestate(role.id)}
+             />
+          <label  style={role.checked?{textDecoration:'line-through'}:null}onDoubleClick={()=>changestate(role.id)}>{role.content}</label>
+          <FaTrash role='button' tabIndex={0
 
-    </div>
+          } onClick={()=>deletefunction(role.id)}/>
+          </li>
+        ))}
+      </ul>
+
+    ):(<p>List is empty</p>)}
+      
+   </main>
   )
 }
-
 export default Content
