54fe27e0c249c4be713b8d737a2e5e1559352328
mukeshr08 <tn65vivasaee@gmail.com>
2025-12-12 23:20:08 +0530

    click events

--- reactbasics/src/Content.js
+++ reactbasics/src/Content.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
 const Content = () => {
-    function changename(){
-        const name=['Earn','Grow','Give']
-        const number=Math.floor(Math.random()*3)
-        return name[number]
+    const names=()=>{
+      const name=['Earn','Grow','Give']
+      const number=Math.floor(Math.random()*3)
+       return name[number]
+    }
+    const clickevent=(e)=>{
+      console.log(e.target.innerHTML)
+      
+      return names
+    }
+    const returnmsg=()=>{
+      console.log("double clicked")
+    }
+    const clickevent1=(name)=>{
+      console.log(`My name is ${name}`)
     }
   return (
     <main>
-        <h3>Let's {changename()} Money</h3>
+        <h3 onDoubleClick={returnmsg}>Let's {names()} Money</h3>
+        <button onClick={(e)=>clickevent(e)}>click me</button>
     </main>
   )
 }
