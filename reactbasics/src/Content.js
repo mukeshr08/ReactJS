56e12127b0e921a01b081e4a9b7e35ee01b9bb0a
mukeshr08 <tn65vivasaee@gmail.com>
2025-12-13 23:41:29 +0530

    useState hooks if,{}

--- reactbasics/src/Content.js
+++ reactbasics/src/Content.js
@@ -1,26 +1,29 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Content = () => {
     const names=()=>{
       const name=['Earn','Grow','Give']
       const number=Math.floor(Math.random()*3)
-       return name[number]
+       setNamechange(name[number])
     }
-    const clickevent=(e)=>{
-      console.log(e.target.innerHTML)
+    const [namechange,setNamechange]=useState('Earn')
+    const [value,setValue]=useState(99)
+    const incrementfunction=()=>{
+      setValue((value)=> {return value+1})//this can be return to the function if it repeat then not specify the initial value on the path
+      setValue(value+1)//value is first value
       
-      return names
     }
-    const returnmsg=()=>{
-      console.log("double clicked")
+    const decrementfunction=()=>{
+      setValue(value-1)
     }
-    const clickevent1=(name)=>{
-      console.log(`My name is ${name}`)
-    }
+    
   return (
     <main>
-        <h3 onDoubleClick={returnmsg}>Let's {names()} Money</h3>
-        <button onClick={(e)=>clickevent(e)}>click me</button>
+        <h3>Let's {namechange} Money</h3>
+        <button onClick={names}>click me</button>
+        <button onClick={decrementfunction}>-</button>
+        <span>{value}</span>
+        <button onClick={incrementfunction}>+</button>
     </main>
   )
 }
